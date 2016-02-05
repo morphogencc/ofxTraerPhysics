fa72fbf0f2783496d861452ea1ee3e96dab6d31c
nathan lachenmyer <nathan@smalldesignfirm.com>
2016-02-05 18:13:18 -0500

    Added noisefield example

--- example-noisefielid/src/ofApp.h
+++ example-noisefielid/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxParticleSystem.h"
 
 class ofApp : public ofBaseApp{
 
@@ -20,5 +21,6 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+protected:
+	std::shared_ptr<ofxParticleSystem> mParticleSystem;
 };
